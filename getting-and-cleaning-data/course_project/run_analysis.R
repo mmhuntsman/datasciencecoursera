9e4acb75542a1c667c1131d752873e59cee7b75e
Matt Huntsman <mhuntsman@ldschurch.org>
2015-06-21 01:05:22 -0600

    Adding a modified version of the r file

--- getting-and-cleaning-data/course_project/run_analysis.R
+++ getting-and-cleaning-data/course_project/run_analysis.R
@@ -1,86 +1,86 @@
 
 # set working directory to file's location
-setwd("D:\\academic\\datasciencecoursera\\getting-and-cleaning-data\\course_project")
+#setwd("D:\\academic\\datasciencecoursera\\getting-and-cleaning-data\\course_project")
 
 
 #Load the features.txt file for use with both the test and train datasets
-featuresDT <- read.table(".\\UCI HAR Dataset\\features.txt")
+featuresDF <- read.table(".\\UCI HAR Dataset\\features.txt")
 
 # Load the activity_labels.txt file for descriptive activity names
-activity_labelsDT <- read.table(".\\UCI HAR Dataset\\activity_labels.txt")
+activity_labelsDF <- read.table(".\\UCI HAR Dataset\\activity_labels.txt")
 
-# Add column names to the activity_labelsDT dataset
-colnames(activity_labelsDT) <- c("ActivityID","ActivityName")
+# Add column names to the activity_labelsDF dataset
+colnames(activity_labelsDF) <- c("ActivityID","ActivityName")
 
 
 
 ############################################
 # Train dataset
 ############################################
-X_trainDT <- read.table(".\\UCI HAR Dataset\\train\\X_train.txt", header=FALSE)
-y_trainDT <- read.table(".\\UCI HAR Dataset\\train\\y_train.txt", header=FALSE)
-subject_trainDT <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt", header=FALSE)
+X_trainDF <- read.table(".\\UCI HAR Dataset\\train\\X_train.txt", header=FALSE)
+y_trainDF <- read.table(".\\UCI HAR Dataset\\train\\y_train.txt", header=FALSE)
+subject_trainDF <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt", header=FALSE)
 
 
-# Add a column header to the y_testDT dataset
-colnames(y_trainDT) <- "ActivityID"
+# Add a column header to the y_testDF dataset
+colnames(y_trainDF) <- "ActivityID"
 
-# Merge the activity_labelsDT with the y_trainDT
+# Merge the activity_labelsDF with the y_trainDF
 # !!!!!! THIS SORTS THE DATA !!!!!!!!!!!!!!!
-# activity_trainDT <- merge(y_trainDT,activity_labelsDT, by="ActivityID")
+# activity_trainDF <- merge(y_trainDF,activity_labelsDF, by="ActivityID")
 
 
-# Add a column header to the subject_testDT dataset
-colnames(subject_trainDT) <- "SubjectID"
+# Add a column header to the subject_testDF dataset
+colnames(subject_trainDF) <- "SubjectID"
 
 # Add the column headers by assigning the values from feature.txt 
-# to the X_trainDT dataset
-colnames(X_trainDT) <- featuresDT[,2]
+# to the X_trainDF dataset
+colnames(X_trainDF) <- featuresDF[,2]
 
 
 # subset the dataset by reducing the number of columns to those 
 # that contain "mean(" and "std("
-x_train_subsetDT <- X_trainDT[,grepl("mean\\(|std\\(",names(X_trainDT))]
+x_train_subsetDF <- X_trainDF[,grepl("mean\\(|std\\(",names(X_trainDF))]
 
-trainDT <- cbind(subject_trainDT, y_trainDT, x_train_subsetDT)
+trainDF <- cbind(subject_trainDF, y_trainDF, x_train_subsetDF)
 
 
 ############################################
 # Test dataset
 ############################################
-X_testDT <- read.table(".\\UCI HAR Dataset\\test\\X_test.txt", header=FALSE)
-y_testDT <- read.table(".\\UCI HAR Dataset\\test\\y_test.txt", header=FALSE)
-subject_testDT <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt", header=FALSE)
+X_testDF <- read.table(".\\UCI HAR Dataset\\test\\X_test.txt", header=FALSE)
+y_testDF <- read.table(".\\UCI HAR Dataset\\test\\y_test.txt", header=FALSE)
+subject_testDF <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt", header=FALSE)
 
-# Add a column header to the y_testDT dataset
-colnames(y_testDT) <- "ActivityID"
+# Add a column header to the y_testDF dataset
+colnames(y_testDF) <- "ActivityID"
 
-# Add a column header to the subject_testDT dataset
-colnames(subject_testDT) <- "SubjectID"
+# Add a column header to the subject_testDF dataset
+colnames(subject_testDF) <- "SubjectID"
 
 # Add the column headers by assigning the values from feature.txt 
-# to the X_testDT dataset
-colnames(X_testDT) <- featuresDT[,2]
+# to the X_testDF dataset
+colnames(X_testDF) <- featuresDF[,2]
 
 
 # subset the dataset by reducing the number of columns to those 
 # that contain "mean(" and "std("
-x_test_subsetDT <- X_testDT[,grepl("mean\\(|std\\(",names(X_testDT))]
+x_test_subsetDF <- X_testDF[,grepl("mean\\(|std\\(",names(X_testDF))]
 
-testDT <- cbind(subject_testDT, y_testDT, x_test_subsetDT)
+testDF <- cbind(subject_testDF, y_testDF, x_test_subsetDF)
 
 
 
 # Merge the training and the test sets to create one data set
-DT <- rbind(trainDT,testDT)
+DF <- rbind(trainDF,testDF)
 
-# Merge the activity_labelsDT with DT for descriptive activity names
+# Merge the activity_labelsDF with DF for descriptive activity names
 # (I've waited until now to merge to the activity lables because the 
 #  merge function messes up the sorting of the datasets)
-DT <- merge(DT,activity_labelsDT, by="ActivityID")
+DF <- merge(DF,activity_labelsDF, by="ActivityID")
 
 # Group data
-GroupDT <- with(DT,aggregate(x=DT[3:68],list(SubjectID=SubjectID,ActivityName=ActivityName), FUN="mean"))
+GroupDF <- with(DF,aggregate(x=DF[3:68],list(SubjectID=SubjectID,ActivityName=ActivityName), FUN="mean"))
 
 # Create txt file
-write.table(GroupDT,file="course_project_step_5_tidy_data_set.txt",row.names=FALSE)
+write.table(GroupDF,file="course_project_step_5_tidy_data_set.txt",row.names=FALSE)
